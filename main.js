68b3a52548d7213287877e92b09caddf34020391
G B <gosha738g@gmail.com>
2022-09-25 19:48:15 +0300

    v.0.1

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 const {google} = require('googleapis');
 const keys = require("./parse xml-xlsx/keys.json");
+const cheerio = require("cheerio");
+const axios = require("axios");
 
 const client = new google.auth.JWT(
     keys.client_email, 
@@ -21,6 +23,8 @@ client.authorize(function(err) {
     }
 });
 
+var parseArray = [];
+
 async function gsrun(cl) {
     const gsapi = google.sheets({version: "v4",auth: cl});
     const readOptions = {
@@ -30,4 +34,36 @@ async function gsrun(cl) {
     let data = await gsapi.spreadsheets.values.get(readOptions);
     let dataArray = data.data.values;
     
+    dataArray.forEach(function(item) {
+        if (item != "") parseArray.push(String(item));
+      });
+    console.log(parseArray);
+    
+    async function parse() {
+        let getHTML = async (url) => {
+            let {data} = await axios.get(url);
+            return cheerio.load(data);
+        };
+        const $ = await getHTML("https://www.ecrs.ru/catalogue/fiskalnye-registratory/evotor-power-fn36.html");
+        let tempPrice = $('div[class=product-page__price]').html();
+        let prices = tempPrice.replace(/[^0-9]/g, '');
+        console.log(prices);
+        
+    }
+    async function parses() {
+        let getHTML = async (url) => {
+            let {data} = await axios.get(url);
+            return cheerio.load(data);
+        };
+        const $ = await getHTML("https://www.ecrs.ru/catalogue/fiskalnye-registratory/evotor-power-1.html");
+        let tempPrice = $('div[class=product-page__price]').html();
+        let prices = tempPrice.replace(/[^0-9]/g, '');
+        console.log(prices);
+        
+    }
+    // for (let indexArray = 0; typeof parseArray[indexArray] != "undefined"; indexArray++) {
+        
+    // }
+    parse()
+    parses()
 }
